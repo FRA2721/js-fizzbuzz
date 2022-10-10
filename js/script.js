c0ee7004b10d37b8bb18e39ac613dd9ace89e2f6
FRA2721 <cimino.francesco36@gmail.com>
2022-10-10 17:12:44 +0200

    css animations cards completed

--- js/script.js
+++ js/script.js
@@ -14,4 +14,11 @@ for (let i = 1; i <= 100; i++) {
     } else {
         output = i;
     };
+    
+    //output-data
+    const schedule = document.createElement("div");
+    schedule.classList.add("schedule");
+    schedule.classList.add(output);
+    schedule.innerHTML += output;
+    row.append(schedule);
 }
