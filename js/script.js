696d6c866812fb2d67f4860f29c9625fd91ec860
FRA2721 <cimino.francesco36@gmail.com>
2022-10-10 16:20:18 +0200

    conditions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert("red");
+const row = document.querySelector(".row");
+
+// loop-numbers-1-to-100
+for (let i = 1; i <= 100; i++) {
+    let output = "";
+
+    // conditions
+    if (i % 3 === 0 && i % 5 === 0) {
+        output = `fizzbuzz`;
+    } else if (i % 3 === 0) {
+        output = `fizz`;
+    } else if (i % 5 === 0){
+        output = `buzz`;
+    } else {
+        output = i;
+    };
+}
